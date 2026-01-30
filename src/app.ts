47a8be92dd50bc7cdd1662168747547eb147278f
Diri03 <diriansyah15@gmai.com>
2026-01-30 23:06:00 +0700

    Tugas Live Session 6

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes";
+import { authenticate } from "./middlewares/auth.middleware";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Sporton Backend API is Running");
+});
+app.get("/auth-middleware", authenticate, (req, res) => {
+  res.send("Tidak Publik");
+});
+export default app;
