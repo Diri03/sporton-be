130a106cd0d1e93fa38d5e5a69872082f9b37e11
Diri03 <diriansyah15@gmai.com>
2026-02-05 08:50:57 +0700

    edit server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,25 @@ dotenv.config();
 const PORT = process.env.PORT || "5001";
 const MONGO_URI = process.env.MONGO_URI || "no-mongo-uri";
 
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(PORT, "0.0.0.0",() => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => console.error("Error connecting to MongoDB:", error));
+
 mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    app.listen(Number(PORT), "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
-  .catch((error) => console.error("Error connecting to MongoDB:", error));
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  });
